17a55eb6d7a3484bdce8e2c37762ed993ce42000
if-shouldrs <if.shouldrs@protonmail.com>
2021-08-04 16:07:57 +0100

    Move to Kurzgesagt's style

---
+++ README.md
@@ -0,0 +1,11 @@
+Kurzgesagt life calendar generator
+====================================
+
+This is a fork of
+[Erik Nyquist](https://github.com/eriknyquist)'s  [Personalised Life Calendar Generator](https://store.waitbutwhy.com/collections/life-calendars).
+
+I modified it to imitate Kurzgesagt's "Calendar of Your Life" since it seemed quite expensive even without shipping.
+
+Currently it generates what I found to be the bare minimum in resemblance to the original and coding quality, and doesn't allow for any customization unlike Erik's version (although I've yet to remove a lot of the code).
+
+For instructions on how to run this please read the [README](https://github.com/eriknyquist/generate_life_calendar/blob/master/README.rst) of the original version. If you have windows and have an "out of memory" error while running, check the [issues](https://github.com/eriknyquist/generate_life_calendar/issues) page.
--- generate_life_calendar.py
+++ generate_life_calendar.py
@@ -1,5 +1,6 @@
 import datetime
 import argparse
+import math
 import sys
 import os
 import cairo
@@ -8,8 +9,23 @@ DOC_WIDTH = 1872  # 26 inches
 DOC_HEIGHT = 2880  # 40 inches
 DOC_NAME = "life_calendar.pdf"
 
-KEY_NEWYEAR_DESC = "First week of the new year"
-KEY_BIRTHDAY_DESC = "Week of your birthday"
+KEY_YEARS_DESC = [
+    "Childhood (0 - 12)",
+    "Adolescence (13 - 19)",
+    "Early Adulthood (20 - 34)",
+    "Middle Adulthood (35 - 49)",
+    "Mature Adulthood (50 - 79)",
+    "Late Adulthood (80 - 100)"
+]
+
+AGE_COLORS = [
+    (0.867, 0.488, 0.293),
+    (0.797, 0.231, 0.276),
+    (0.673, 0.101, 0.334),
+    (0.523, 0.096, 0.659),
+    (0.016, 0.374, 0.782),
+    (0.066, 0.737, 0.22)
+]
 
 XAXIS_DESC = "Weeks of the year"
 YAXIS_DESC = "Years of your life"
@@ -22,7 +38,7 @@ TINYFONT_SIZE = 14
 MAX_TITLE_SIZE = 30
 DEFAULT_TITLE = "LIFE CALENDAR"
 
-NUM_ROWS = 90
+NUM_ROWS = 100
 NUM_COLUMNS = 52
 
 Y_MARGIN = 144
@@ -31,9 +47,6 @@ BOX_MARGIN = 6
 BOX_LINE_WIDTH = 3
 BOX_SIZE = ((DOC_HEIGHT - (Y_MARGIN + 36)) / NUM_ROWS) - BOX_MARGIN
 X_MARGIN = (DOC_WIDTH - ((BOX_SIZE + BOX_MARGIN) * NUM_COLUMNS)) / 2
-
-BIRTHDAY_COLOUR = (0.5, 0.5, 0.5)
-NEWYEAR_COLOUR = (0.8, 0.8, 0.8)
 
 ARROW_HEAD_LENGTH = 36
 ARROW_HEAD_WIDTH = 8
@@ -53,20 +66,20 @@ def parse_date(date):
 
     raise ValueError("Incorrect date format: must be dd-mm-yyyy or dd/mm/yyyy")
 
-def draw_square(ctx, pos_x, pos_y, fillcolour=(1, 1, 1)):
 
+def draw_square(ctx, pos_x, pos_y, color=(1, 1, 1)):
     """
     Draws a square at pos_x,pos_y
     """
 
     ctx.set_line_width(BOX_LINE_WIDTH)
-    ctx.set_source_rgb(0, 0, 0)
-    ctx.move_to(pos_x, pos_y)
+    ctx.set_source_rgb(*color)
+    ctx.move_to(pos_x + BOX_SIZE / 2, pos_y)
 
-    ctx.rectangle(pos_x, pos_y, BOX_SIZE, BOX_SIZE)
+    ctx.arc(pos_x, pos_y, BOX_SIZE / 2, 0, 2 * math.pi)
     ctx.stroke_preserve()
 
-    ctx.set_source_rgb(*fillcolour)
+    ctx.set_source_rgba(0, 0, 0, 0)
     ctx.fill()
 
 
@@ -82,29 +95,24 @@ def is_current_week(now, month, day):
 
     return (now <= date1 < end) or (now <= date2 < end)
 
-def draw_row(ctx, pos_y, birthdate, date):
 
+def draw_row(ctx, pos_y, birthdate, date, color):
     """
     Draws a row of 52 squares, starting at pos_y
     """
 
-    pos_x = X_MARGIN
+    pos_x = X_MARGIN + BOX_SIZE / 2
 
     for i in range(NUM_COLUMNS):
-        fill=(1, 1, 1)
+        fill = color
 
-        if is_current_week(date, birthdate.month, birthdate.day):
-            fill = BIRTHDAY_COLOUR
-        elif is_current_week(date, 1, 1):
-            fill = NEWYEAR_COLOUR
-
-        draw_square(ctx, pos_x, pos_y, fillcolour=fill)
+        draw_square(ctx, pos_x, pos_y, color=fill)
         pos_x += BOX_SIZE + BOX_MARGIN
         date += datetime.timedelta(weeks=1)
 
 
 def draw_key_item(ctx, pos_x, pos_y, desc, colour):
-    draw_square(ctx, pos_x, pos_y, fillcolour=colour)
+    draw_square(ctx, pos_x + BOX_SIZE / 2, pos_y + BOX_SIZE / 2, color=colour)
     pos_x += BOX_SIZE + (BOX_SIZE / 2)
 
     ctx.set_source_rgb(0, 0, 0)
@@ -115,21 +123,35 @@ def draw_key_item(ctx, pos_x, pos_y, desc, colour):
     return pos_x + w + (BOX_SIZE * 2)
 
 
+def get_color(year):
+    if year < 13:
+        return AGE_COLORS[0]
+    if year < 20:
+        return AGE_COLORS[1]
+    if year < 35:
+        return AGE_COLORS[2]
+    if year < 50:
+        return AGE_COLORS[3]
+    if year < 80:
+        return AGE_COLORS[4]
+    return AGE_COLORS[5]
+
+
 def draw_grid(ctx, date, birthdate):
     """
     Draws the whole grid of 52x90 squares
     """
     start_date = date
-    pos_x = X_MARGIN / 4
-    pos_y = pos_x
+    pos_x = X_MARGIN
+    pos_y = Y_MARGIN - BOX_SIZE * 3.5
 
-    # Draw the key for box colours
+    # Draw the key for circle colours
     ctx.set_font_size(TINYFONT_SIZE)
     ctx.select_font_face(FONT, cairo.FONT_SLANT_NORMAL,
-        cairo.FONT_WEIGHT_NORMAL)
+                         cairo.FONT_WEIGHT_NORMAL)
 
-    pos_x = draw_key_item(ctx, pos_x, pos_y, KEY_BIRTHDAY_DESC, BIRTHDAY_COLOUR)
-    draw_key_item(ctx, pos_x, pos_y, KEY_NEWYEAR_DESC, NEWYEAR_COLOUR)
+    for i in range(len(KEY_YEARS_DESC)):
+        pos_x = draw_key_item(ctx, pos_x, pos_y, KEY_YEARS_DESC[i], AGE_COLORS[i])
 
     # draw week numbers above top row
     ctx.set_font_size(TINYFONT_SIZE)
@@ -149,19 +171,19 @@ def draw_grid(ctx, date, birthdate):
     ctx.select_font_face(FONT, cairo.FONT_SLANT_ITALIC,
                          cairo.FONT_WEIGHT_NORMAL)
 
-    for i in range(NUM_ROWS):
+    for i in range(1, NUM_ROWS + 1):
         # Generate string for current date
         ctx.set_source_rgb(0, 0, 0)
-        date_str = date.strftime('%d %b, %Y')
+        date_str = f'{i}'
         w, h = text_size(ctx, date_str)
 
         # Draw it in front of the current row
         ctx.move_to(X_MARGIN - w - BOX_SIZE,
-            pos_y + ((BOX_SIZE / 2) + (h / 2)))
+                    pos_y + ((BOX_SIZE / 2) + (h / 2)))
         ctx.show_text(date_str)
 
         # Draw the current row
-        draw_row(ctx, pos_y, birthdate, date)
+        draw_row(ctx, pos_y + BOX_SIZE / 2, birthdate, date, get_color(i))
 
         # Increment y position and current date by 1 row/year
         pos_y += BOX_SIZE + BOX_MARGIN
@@ -211,6 +233,7 @@ def parse_date(date):
             return ret
 
     raise argparse.ArgumentTypeError("incorrect date format")
+
 
 def main():
     parser = argparse.ArgumentParser(description='\nGenerate a personalized "Life '
